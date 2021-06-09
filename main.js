9e5920930443d7ca0384278a488b02785b9b40bb
preetithirukonda <51178331+preetithirukonda@users.noreply.github.com>
2021-06-08 22:15:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
 
-ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight + "px";
-})
-
-setTimeout(function() {
-    $('#demo-modal').modal();
-}, 500);
